b4b0fc7dff43a8952f3025eb4822ed72b3abd583
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2025-10-19 23:23:54 +0100

    main

--- SomniaDataStream/src/lib/streams.ts
+++
@@ -1,7 +0,0 @@
-import { SDK } from '@somnia-chain/streams'
-import { publicClient, walletClient } from './viem'
-
-export const sdk = new SDK({
-  public: publicClient,
-  wallet: walletClient ?? undefined,
-})
